4c9203023ff1b8aedf5e859a986d04202622e52d
wswind <18399096+wswind@users.noreply.github.com>
2019-07-29 22:28:03 +0800

    init

---
+++ ClientTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
